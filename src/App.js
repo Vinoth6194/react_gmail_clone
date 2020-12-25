a5d9f0938976a5dfc04ece2881f4ef99c10eb258
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-12-25 14:14:17 +0530

    material ui installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
+      <Header></Header>
       <h2>Gmail Clone</h2>
     </div>
   );
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./header.css";
+function Header() {
+  return <div className="header">This is a header</div>;
+}
+
+export default Header;
