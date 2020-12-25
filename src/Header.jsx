28e95ef44f0376dccfdbff637fb09dae08eb6c22
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-12-25 14:29:14 +0530

    header__left

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
 function Header() {
-  return <div className="header">This is a header</div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+        />
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  );
 }
 
 export default Header;
