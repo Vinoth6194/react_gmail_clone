ab4f7463f0059232c403bb78a708a110dff39b03
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-12-25 13:46:12 +0530

    preferences rework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h2>Gmail Clone</h2>
     </div>
   );
