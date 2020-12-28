8929a4ab3efdf1c11da081a3db19cb1cee4a10c1
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-12-28 18:03:52 +0530

    routing setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import EmailList from "./EmailList";
+import Mail from "./Mail";
 function App() {
   return (
-    <div className="app">
-      <Header></Header>
-      <Sidebar />
-      {/* <h2>Gmail Clone</h2> */}
-    </div>
+    <Router>
+      <div className="app">
+        <Header></Header>
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
