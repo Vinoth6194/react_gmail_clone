8e6298c9a05b288939de6cd481705145d737e7b2
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-12-29 19:25:46 +0530

    emaillist settings right

--- src/EmailList.jsx
+++ src/EmailList.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import RedoIcon from "@material-ui/icons/Redo";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import SettingsIcon from "@material-ui/icons/Settings";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
 import "./emailList.css";
 function EmailList() {
   return (
@@ -18,6 +22,20 @@ function EmailList() {
           </IconButton>
           <IconButton>
             <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
           </IconButton>
         </div>
       </div>
