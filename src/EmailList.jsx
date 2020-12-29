88e598e6af5615a01663ff195bd377a7155c684c
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-12-29 19:19:08 +0530

    emaillist settigs left icons

---
+++ src/EmailList.jsx
@@ -0,0 +1,28 @@
+import { Checkbox, IconButton } from "@material-ui/core";
+import React from "react";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import RedoIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import "./emailList.css";
+function EmailList() {
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EmailList;
---
+++ src/Mail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./mail.css";
+function Mail() {
+  return <div className="mail"></div>;
+}
+
+export default Mail;
