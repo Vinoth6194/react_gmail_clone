3df23b7dd7e717b686ba0859be087fd48e177454
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-12-31 17:25:24 +0530

    left side icone and routing to inbox

--- src/Mail.jsx
+++ src/Mail.jsx
@@ -1,7 +1,55 @@
+import { IconButton } from "@material-ui/core";
 import React from "react";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 import "./mail.css";
+import { useHistory } from "react-router-dom";
 function Mail() {
-  return <div className="mail">Your email</div>;
+  const history = useHistory();
+  return (
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__tolleLeft">
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail__tollsRight"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Mail;
