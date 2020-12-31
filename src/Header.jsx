204164f3bc99acbdadc98140691918ae53e50400
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-01 01:47:11 +0530

    signout the google auth

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,10 +6,19 @@ import SearchIcon from "@material-ui/icons/Search";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import { selectUser } from "./features/userSlice";
-import { useSelector } from "react-redux";
+import { logout, selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "./firebase";
+
 function Header() {
   const user = useSelector(selectUser);
+
+  const dispatch = useDispatch();
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(logout());
+    });
+  };
   return (
     <div className="header">
       <div className="header__left">
@@ -33,7 +42,7 @@ function Header() {
         <IconButton>
           <NotificationsIcon />
         </IconButton>
-        <Avatar src={user?.photoUrl} />
+        <Avatar src={user?.photoUrl} onClick={signOut} />
       </div>
     </div>
   );
