d84d4f73a830daa31618b88068879eb2544efe54
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-01 01:31:39 +0530

    user signedin with google

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,26 @@
 import { Button } from "@material-ui/core";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/userSlice";
+import { auth, provider } from "./firebase";
 import "./login.css";
 
 function Login() {
-  const signIn = () => {};
+  const dispatch = useDispatch();
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(({ user }) =>
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        )
+      )
+      .catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <div className="login__container">
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -17,6 +17,6 @@ export const userSlice = createSlice({
 
 export const { login, logout } = userSlice.actions;
 
-export const selectUser = (state) => state.mail.user;
+export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
