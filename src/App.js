647a888174ae0732e9dc7da59f8e205a3b480721
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-01 01:34:27 +0530

    user persisted for refresh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
@@ -6,13 +6,29 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import EmailList from "./EmailList";
 import Mail from "./Mail";
 import SendMail from "./SendMail";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
 import { login, selectUser } from "./features/userSlice";
 import Login from "./Login";
+import { auth } from "./firebase";
+
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      }
+    });
+  }, []);
   return (
     <Router>
       {!user ? (
