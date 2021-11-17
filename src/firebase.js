b3f508d7e119d64dc3c4a5d63c78dbcaa080529f
Canarys-Vinoth Kumar <vinothhkumar.cse.s@gmail.com>
2021-11-17 15:54:46 +0530

    Re confiugured the fireabse config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDL4-pkODuIvdydjzD520o1--DDmKZ5UI4",
-  authDomain: "react--clone-8aa8a.firebaseapp.com",
-  projectId: "react--clone-8aa8a",
-  storageBucket: "react--clone-8aa8a.appspot.com",
-  messagingSenderId: "995736285710",
-  appId: "1:995736285710:web:7675ab67e19f4be1c7025f",
-  measurementId: "G-V6S3C3R2N4",
+  apiKey: "AIzaSyBhmb-VDv-TelcleUxXXmEED1nGb70vB_Y",
+  authDomain: "react--clone-5e7f3.firebaseapp.com",
+  projectId: "react--clone-5e7f3",
+  storageBucket: "react--clone-5e7f3.appspot.com",
+  messagingSenderId: "486793875137",
+  appId: "1:486793875137:web:134ace4d73f49a805fce40",
+  measurementId: "G-94HCMPFQNC"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
